db1f7080ebfa9cf83e2c105bc71f8c995839afff
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-11-26 11:06:56 +0100

    test: repository user

---
+++ twitterapp/test/integration/repository/user_repository_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class UserRepositoryTestCase(unittest.TestCase):
+
+
+
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def test_create_user(self):
+
+
+if __name__ == '__main__':
+    unittest.main()
