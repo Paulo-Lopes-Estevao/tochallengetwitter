d3d5bf3fc009932642d2d0bde2783107f61cdba0
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-11-26 16:58:22 +0100

    test: create tweets passed :ok

--- twitterapp/test/integration/repository/tweet_repository_test.py
+++ twitterapp/test/integration/repository/tweet_repository_test.py
@@ -1,9 +1,29 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+import django
+
+django.setup()
+from django.core.management import call_command
+
 import unittest
+
+from twitterapp.repository.tweet_repository import tweetRepository
+from twitterapp.usecase.tweet_usecase import tweetUsecase
 
 
 class TweetRepositoryTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+    def setUp(self):
+        self.tweet_usecase = tweetUsecase(tweetRepository=tweetRepository)
+
+    def test_create_tweets(self):
+        data_tweet = {"user":"ef5da2dc-3b46-4d30-92f2-154f6f75f1ad", "description": 'Olá primeiro tweets', 'midia': '/source/test.png'}
+        tweet = self.tweet_usecase.addTweets(**data_tweet)
+        self.assertGreaterEqual(len(tweet), 1, "tweets não publicada cadastrado")
+
+    def test_deactivate_tweets(self):
+        pass
 
 
 if __name__ == '__main__':
