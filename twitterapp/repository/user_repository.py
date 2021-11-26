75f54663e78230d1b051e0bd4996841dbd54f802
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-11-26 15:13:11 +0100

    update: get all user repository

--- twitterapp/repository/user_repository.py
+++ twitterapp/repository/user_repository.py
@@ -6,10 +6,10 @@ from twitterapp.models import User
 class userRepository():
 
     def __init__(self,User: Type[User]):
-        self.user = User()
+        self.user = User
 
-    def findall_users(self) -> User:
-        return self.user
+    def findall_users(self)-> User:
+        return self.user.filter(state=True).values()
 
     def findbyid_users(self,id: int)-> User:
         users = self.user.objects.filter(id=id)
@@ -18,12 +18,13 @@ class userRepository():
 
     def create_user(self, **users) -> User:
         try:
-            self.user.name = users.get('name')
-            self.user.telefone = users.get('telefone')
-            self.user.email = users.get('email')
-            self.user.password = users.get('password')
-            user = self.user.save()
-            return  user
+            user =self.user()
+            user.name = users.get('name')
+            user.telefone = users.get('telefone')
+            user.email = users.get('email')
+            user.password = users.get('password')
+            result_user = user.save()
+            return  result_user
         except Exception as e:
             raise e
 
