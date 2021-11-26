7441b5f9add9b7ee6f82772c8eab98f88db197ba
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-11-26 14:20:38 +0100

    test: create user passed :ok

--- twitterapp/test/integration/repository/user_repository_test.py
+++ twitterapp/test/integration/repository/user_repository_test.py
@@ -1,14 +1,25 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+import django
+
+django.setup()
+from django.core.management import call_command
 import unittest
+
+from twitterapp.usecase.interactor.user_interactor import userInteractor
+from twitterapp.repository.user_repository import userRepository
 
 
 class UserRepositoryTestCase(unittest.TestCase):
 
-
-
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def setUp(self):
+        self.user_interactor = userInteractor(userRepository=userRepository)
 
     def test_create_user(self):
+        data_user = {"name": 'John', 'telefone': '923453925', 'email': "pl@gmail.com", 'password': '1234'}
+        user = self.user_interactor.addUsers(**data_user)
+        self.assertEqual(user.get('email'), data_user.get('email'), "usuario cadastrado")
 
 
 if __name__ == '__main__':
