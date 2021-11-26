8dcf12d3b10dae48b96a6c134a93ddc2996ec38c
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-11-26 18:46:18 +0100

    update: add new field

--- twitterapp/models.py
+++ twitterapp/models.py
@@ -5,12 +5,16 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=45,unique=True,blank=True, null=True)
     telefone = models.CharField(db_column='telefone', max_length=12, blank=True, null=True)
+    email = models.EmailField(db_column='email', max_length=255,unique=True, blank=True, null=True)
+    username = models.CharField(max_length=30, unique=False)
     state = models.BooleanField(default=True, null=True)
     creation_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated_date = models.DateTimeField(auto_now=True, blank=True, null=True)
 
 
+    USERNAME_FIELD = 'name'
     REQUIRED_FIELDS = ('email',)
     def __str__(self):
         return self.username
@@ -22,7 +26,7 @@ class Tweet(models.Model):
     midia = models.TextField(blank=True, null=True)
     emoji = models.TextField(blank=True, null=True)
     gif = models.TextField(blank=True, null=True)
-    reation = models.IntegerField(max_length=1,blank=True, null=True)
+    reation = models.IntegerField(blank=True, null=True)
     state = models.BooleanField(default=True, null=True)
     creation_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated_date = models.DateTimeField(auto_now=True, blank=True, null=True)
@@ -35,6 +39,7 @@ class Retweet(models.Model):
     midia = models.TextField(blank=True, null=True)
     emoji = models.TextField(blank=True, null=True)
     gif = models.TextField(blank=True, null=True)
+    reation = models.IntegerField(blank=True, null=True)
     state = models.BooleanField(default=True, null=True)
     creation_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
     updated_date = models.DateTimeField(auto_now=True, blank=True, null=True)
