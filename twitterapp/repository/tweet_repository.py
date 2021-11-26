9388d8ea8c2b52622ce01c9c2ad59627626ac886
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-11-26 17:03:07 +0100

    refator: repository tweets

--- twitterapp/repository/tweet_repository.py
+++ twitterapp/repository/tweet_repository.py
@@ -1,29 +1,28 @@
-
 from typing import Type
 from twitterapp.models import Tweet
 
 
 class tweetRepository():
 
-    def __init__(self,Tweet: Type[Tweet]):
+    def __init__(self, Tweet: Type[Tweet]):
         self.tweet = Tweet
 
-    def findall_tweets(self):
-        return self.tweet
+    def findall_tweets(self) -> Tweet:
+        return self.tweet.filter(state=True).values()
 
-    def findbyid_tweets(self,id: int):
+    def findbyid_tweets(self, id: int) -> Tweet:
         tweets = self.tweet.objects.filter(id=id)
         return tweets
 
+    def create_tweet(self, **tweets) -> Tweet:
+        tweet = self.tweet()
+        tweet.user_id = tweets.get('user')
+        tweet.description = tweets.get('description')
+        tweet.midia = tweets.get('midia',None)
+        tweet.emoji = tweets.get('emoji',None)
+        tweet.gif = tweets.get('gif',None)
+        result_tweet = tweet.save()
+        return result_tweet
 
-    def create_tweet(self, **tweet):
-        self.tweet.user_id = tweet.get('user_id')
-        self.tweet.description = tweet.get('description')
-        self.tweet.midia = tweet.get('midia')
-        self.tweet.emoji = tweet.get('emoji')
-        self.tweet.gif = tweet.get('gif')
-        self.tweet.reation = tweet.get('reation')
-        return self.tweet.save()
-
-    def delete_tweet(self, id):
-        self.tweet.objects.filter(id=id).update(state=False)
+    def delete_tweet(self, id) -> Tweet:
+        return self.tweet.objects.filter(id=id).update(state=False)
