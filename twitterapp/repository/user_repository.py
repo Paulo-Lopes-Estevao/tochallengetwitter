5cd167d811f66280ea62774eaf68475aa69430c1
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-11-26 14:21:22 +0100

    refator: user repository

--- twitterapp/repository/user_repository.py
+++ twitterapp/repository/user_repository.py
@@ -6,20 +6,26 @@ from twitterapp.models import User
 class userRepository():
 
     def __init__(self,User: Type[User]):
-        self.user = User
+        self.user = User()
 
-    def findall_users(self):
+    def findall_users(self) -> User:
         return self.user
 
-    def findbyid_users(self,id: int):
+    def findbyid_users(self,id: int)-> User:
         users = self.user.objects.filter(id=id)
         return users
 
 
-    def create_user(self, **users):
-        self.user.name = users.get('name')
-        self.user.telefone = users.get('telefone')
-        return self.user.save()
+    def create_user(self, **users) -> User:
+        try:
+            self.user.name = users.get('name')
+            self.user.telefone = users.get('telefone')
+            self.user.email = users.get('email')
+            self.user.password = users.get('password')
+            user = self.user.save()
+            return  user
+        except Exception as e:
+            raise e
 
-    def delete_user(self, id):
-        self.user.objects.filter(id=id).update(state=False)
+    def delete_user(self, id)-> User:
+        return self.user.objects.filter(id=id).update(state=False)
