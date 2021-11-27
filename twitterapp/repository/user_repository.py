c0a345759fc992884db3aacd74db0f7f17c41a34
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-11-27 07:29:33 +0100

    update: repository user

--- twitterapp/repository/user_repository.py
+++ twitterapp/repository/user_repository.py
@@ -1,32 +1,31 @@
-
 from typing import Type
 from twitterapp.models import User
 
 
 class userRepository():
 
-    def __init__(self,User: Type[User]):
+    def __init__(self, User: Type[User]):
         self.user = User
 
-    def findall_users(self)-> User:
-        return self.user.filter(state=True).values()
+    def findall_users(self) -> User:
+        users = self.user.objects.filter(state=True).values()
+        return users
 
-    def findbyid_users(self,id)-> User:
+    def findbyid_users(self, id) -> User:
         users = self.user.objects.filter(id=id)
         return users
 
-
     def create_user(self, **users) -> User:
         try:
-            user =self.user()
+            user = self.user()
             user.name = users.get('name')
             user.telefone = users.get('telefone')
             user.email = users.get('email')
             user.password = users.get('password')
             result_user = user.save()
-            return  result_user
+            return result_user
         except Exception as e:
             raise e
 
-    def delete_user(self, id)-> User:
+    def delete_user(self, id) -> User:
         return self.user.objects.filter(id=id).update(state=False)
